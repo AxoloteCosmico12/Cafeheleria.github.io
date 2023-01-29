888ac84e8309a690475f09a0416264341ed62127
AxoloteCosmico12 <123844893+AxoloteCosmico12@users.noreply.github.com>
2023-01-28 20:25:39 -0600

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-apiKey: "AIzaSyDtohQgIk-MT_ZySpNjCuvLnulOb-VLImg",
-  authDomain: "cafeheleria.firebaseapp.com",
-  projectId: "cafeheleria",
-  storageBucket: "cafeheleria.appspot.com",
-  messagingSenderId: "469502901568",
-  appId: "1:469502901568:web:e42202cb65f6872c8b0ea5",
-  measurementId: "G-SFKB4KJQR7"
-});
